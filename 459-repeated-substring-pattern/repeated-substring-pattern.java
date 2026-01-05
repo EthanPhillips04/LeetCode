9d79031a9c79273e81f2f5957f8bf5cb80af96e7
Ethan Phillips <ethanphillips81@gmail.com>
2026-01-05 11:30:14 -0500

    Time: 78 ms (13.47%) | Memory: 47.2 MB (10.25%) - LeetSync

---
+++ 459-repeated-substring-pattern/repeated-substring-pattern.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        int size = s.length();
+
+        String fold = s.substring(1, size) + s.substring(0, size - 1);
+
+        return fold.contains(s);
+    }
+}
