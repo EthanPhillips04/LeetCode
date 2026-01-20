635ff111d74666919b82db3647ffa883501565ae
Ethan Phillips <ethanphillips81@gmail.com>
2026-01-20 11:19:49 -0500

    Time: 1 ms (77.37%) | Memory: 42.5 MB (54.70%) - LeetSync

---
+++ 202-happy-number/happy-number.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isHappy(int n) {
+        
+        int slow = n;
+        int fast = n;
+
+        do {
+            slow = square(slow);
+            fast = square(square(fast));
+        } while (slow != fast);
+
+        return slow == 1;
+    }
+
+    public int square(int num) {
+        int ans = 0;
+
+        while (num > 0) {
+            int r = num % 10;
+            ans += r * r;
+            num /= 10;
+        }
+
+        return ans;
+    }
+}
